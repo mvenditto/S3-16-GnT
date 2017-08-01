afc3ce80fe5d5dbe4727efef0190cd9c86563b50
mvenditto <matteo.venditto@studio.unibo.it>
2017-08-01 16:25:05 +0200

    first commit

---
+++ desktop/src/com/unibo/s3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.unibo.s3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unibo.s3.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
