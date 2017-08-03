edce141b57267aff1f53caadce56fddbb8c67485
UTENTE <daniele.rosetti@studio.unibo.it>
2017-08-03 12:35:36 +0200

    Refactored Actors

--- core/src/daniele/comunication/RemoteActor.java
+++ core/src/daniele/comunication/RemoteActor.java
@@ -21,24 +21,6 @@ public class RemoteActor extends UntypedAbstractActor   {
             System.out.println("error");
         }
     }
-
-    public static void main(String[] args) {
-        try {
-            System.out.println("ip address: " + Inet4Address.getLocalHost().getHostAddress());
-            //parte per ottenere dinamicamente l'ip
-            Config customConf = ConfigFactory.parseString(
-                    "{\"akka\":{\"actor\":{\"provider\":\"akka.remote.RemoteActorRefProvider\"}," +
-                            "\"loglevel\":\"INFO\",\"remote\":{\"enabled-transports\":[\"akka.remote.netty.tcp\"]" +
-                            ",\"log-received-messages\":\"on\",\"log-sent-messages\":\"on\"" +
-                            ",\"netty\":{\"tcp\":{\"hostname\":\""+Inet4Address.getLocalHost().getHostAddress()+"\",\"port\":2727}}}}}"
-            );
-            ActorSystem system = ActorSystem.create("RemoteSystem", customConf);
-            system.actorOf(Props.create(RemoteActor.class), "remote");
-            System.out.println("remote is ready");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
 }
 
 
