46e197b53538095189763fe7ec3164ed310a7b14
UTENTE <daniele.rosetti@studio.unibo.it>
2017-08-03 19:47:25 +0200

    created launcher for two systems

---
+++ core/src/daniele/comunication/LocalLauncher.scala
@@ -0,0 +1,33 @@
+package daniele.comunication
+
+import java.net.InetAddress
+
+import akka.actor.{ActorRef, ActorSelection, Props}
+import com.typesafe.config.{Config, ConfigFactory}
+import daniele.utils.IntMsg
+
+object RemoteLauncher extends App {
+  val confText: String =
+    "{\"akka\":{\"actor\":{\"provider\":\"akka.remote.RemoteActorRefProvider\"}," +
+      "\"loglevel\":\"INFO\",\"remote\":{\"enabled-transports\":[\"akka.remote.netty.tcp\"]" +
+      ",\"log-received-messages\":\"on\",\"log-sent-messages\":\"on\"" +
+      ",\"netty\":{\"tcp\":{\"hostname\":\""+ InetAddress.getLocalHost.getHostAddress+"\",\"port\":2727}}}}}"
+  val customConf: Config = ConfigFactory.parseString(confText)
+  SystemManager.getInstance().createSystem("RemoteSystem", customConf)
+  SystemManager.getInstance().getSystem("RemoteSystem").actorOf(Props[RemoteActor], "remote")
+  println("remote ready, ip: " + InetAddress.getLocalHost.getHostAddress)
+}
+
+object LocalLauncher extends App {
+  val confText: String =
+    "{\"akka\":{\"actor\":{\"provider\":\"akka.remote.RemoteActorRefProvider\"}," +
+      "\"loglevel\":\"INFO\",\"remote\":{\"enabled-transports\":[\"akka.remote.netty.tcp\"]" +
+      ",\"log-received-messages\":\"on\",\"log-sent-messages\":\"on\"" +
+      ",\"netty\":{\"tcp\":{\"hostname\":\""+ InetAddress.getLocalHost.getHostAddress+"\",\"port\":5050}}}}}"
+  val customConf: Config = ConfigFactory.parseString(confText)
+  SystemManager.getInstance().createSystem("LocalSystem", customConf)
+  val local: ActorRef = SystemManager.getInstance().getSystem("LocalSystem").actorOf(Props[RemoteActor], "local")
+  val remoteActor: ActorSelection = SystemManager.getInstance().getSystem("LocalSystem").actorSelection("akka.tcp://RemoteSystem@192.168.1.12:2727/user/remote")
+  println("That 's remote: " + remoteActor)
+  remoteActor.tell(new IntMsg(0), local)
+}
