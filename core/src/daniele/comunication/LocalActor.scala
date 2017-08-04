28d8d3c1275460aba6f1bbd2689acdff8ef46c4c
UTENTE <daniele.rosetti@studio.unibo.it>
2017-08-04 16:51:12 +0200

    Add factory methods to LocalActor and add constructor to set Actor name

--- core/src/daniele/comunication/LocalActor.scala
+++ core/src/daniele/comunication/LocalActor.scala
@@ -1,16 +1,20 @@
 package daniele.comunication
 
-import akka.actor.UntypedAbstractActor
+import akka.actor.{Props, UntypedAbstractActor}
 import daniele.utils.IntMsg
 
 
-class LocalActor extends UntypedAbstractActor {
+class LocalActor(name: String) extends UntypedAbstractActor {
 
   override def onReceive(message: Any): Unit = message match {
     case x: IntMsg =>
-      println("local: " + x.getVal + " from: " + sender())
+      println(name + ": " + x.getVal + " from: " + sender())
       x inc()
       sender().tell(new IntMsg(x.getVal), getSelf())
     case _ => println("message unknown")
   }
+}
+
+object LocalActor {
+  def props(name: String): Props = Props(new LocalActor(name))
 }
