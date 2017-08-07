edb687da0d3a7d6f2cc87539fdfe0784857a66db
UTENTE <daniele.rosetti@studio.unibo.it>
2017-08-07 22:29:48 +0200

    Created test for reference an Actor after its creation

--- core/src/daniele/comunication/DistributedTest.java
+++ core/src/daniele/comunication/DistributedTest.java
@@ -1,12 +1,15 @@
 package daniele.comunication;
 
-import akka.actor.ActorRef;
+import akka.actor.*;
+import akka.util.Timeout;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import daniele.comunication.Messages.IntMsg;
 import org.junit.Test;
+import scala.concurrent.Future;
 
 import java.net.Inet4Address;
+import java.util.concurrent.TimeUnit;
 
 public class DistributedTest {
 
@@ -43,27 +46,17 @@ public class DistributedTest {
     }
 
     @Test
-    public void testTwoSystemsThreeActors() {
-        try {
-            String confText =
-                    "{\"akka\":{\"actor\":{\"provider\":\"akka.remote.RemoteActorRefProvider\"}," +
-                            "\"loglevel\":\"INFO\",\"remote\":{\"enabled-transports\":[\"akka.remote.netty.tcp\"]" +
-                            ",\"log-received-messages\":\"on\",\"log-sent-messages\":\"on\"" +
-                            ",\"netty\":{\"tcp\":{\"hostname\":\""+ Inet4Address.getLocalHost().getHostAddress()+"\",\"port\":2727}}}}}";
-            Config customConf = ConfigFactory.parseString(confText);
-            SystemManager.getInstance().createSystem("RemoteSystem", customConf);
-            ActorRef remote = SystemManager.getInstance().getSystem("RemoteSystem").actorOf
-                    (IntActor.props("Remote"), "remote");
-            customConf = ConfigFactory.parseString(confText.replace("2727", "5050"));
-            SystemManager.getInstance().createSystem("LocalSystem", customConf);
-            ActorRef firstLocal = SystemManager.getInstance().getSystem("LocalSystem").actorOf
-                    (AllActor.props("First Local"), "firstLocal");
-            ActorRef secondLocal = SystemManager.getInstance().getSystem("LocalSystem").actorOf
-                    (StringActor.props("Second Local"), "secondLocal");
-            remote.tell(new IntMsg(0), firstLocal);
-            secondLocal.tell(new Messages.StringMsg("hello world"), firstLocal);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void testSelectionOnLocalSystem() {
+        SystemManager.getInstance().createSystem("System", null);
+        SystemManager.getInstance().getSystem("System").actorOf
+                (IntActor.props("First Local"), "firstLocal");
+        SystemManager.getInstance().getSystem("System").actorOf
+                (IntActor.props("Second Local"), "secondLocal");
+        ActorSelection second = SystemManager.getInstance().getSystem("System").actorSelection("akka://System/user/secondLocal");
+        Future<ActorRef> future = SystemManager.getInstance().getSystem("System")
+                .actorSelection("/user/firstLocal").resolveOne(new Timeout(5, TimeUnit.SECONDS).duration());
+        second.tell(new IntMsg(0), future.value().get().get());
     }
+
+
 }
