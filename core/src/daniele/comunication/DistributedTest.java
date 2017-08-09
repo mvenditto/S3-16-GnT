83e3a72c88771c319ad638057f1781a8bb7771d6
UTENTE <daniele.rosetti@studio.unibo.it>
2017-08-09 20:44:03 +0200

    Create methods to manipulate ActorSystem and refactored test

--- core/src/daniele/comunication/DistributedTest.java
+++ core/src/daniele/comunication/DistributedTest.java
@@ -1,15 +1,12 @@
 package daniele.comunication;
 
 import akka.actor.*;
-import akka.util.Timeout;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import daniele.comunication.Messages.IntMsg;
 import org.junit.Test;
-import scala.concurrent.Future;
 
 import java.net.Inet4Address;
-import java.util.concurrent.TimeUnit;
 
 public class DistributedTest {
 
@@ -23,11 +20,10 @@ public class DistributedTest {
                             ",\"netty\":{\"tcp\":{\"hostname\":\""+ Inet4Address.getLocalHost().getHostAddress()+"\",\"port\":2727}}}}}";
             Config customConf = ConfigFactory.parseString(confText);
             SystemManager.getInstance().createSystem("RemoteSystem", customConf);
-            ActorRef remote = SystemManager.getInstance().getSystem("RemoteSystem").actorOf
-                    (IntActor.props("Remote"), "remote");
+            ActorRef remote = SystemManager.getInstance().createActor(IntActor.props("Remote"), "remote");
             customConf = ConfigFactory.parseString(confText.replace("2727", "5050"));
-            SystemManager.getInstance().createSystem("LocalSystem", customConf);
-            ActorRef local = SystemManager.getInstance().getSystem("LocalSystem").actorOf
+            ActorSystem RemoteSystem = ActorSystem.create("LocalSystem", customConf);
+            ActorRef local = RemoteSystem.actorOf
                     (IntActor.props("Local"), "local");
             remote.tell(new IntMsg(0), local);
         } catch (Exception e) {
@@ -38,24 +34,19 @@ public class DistributedTest {
     @Test
     public void testCommunicationBetweenSameSystem() {
         SystemManager.getInstance().createSystem("LocalSystem", null);
-        ActorRef firstLocal = SystemManager.getInstance().getSystem("LocalSystem").actorOf
-                (IntActor.props("First Local"), "firstLocal");
-        ActorRef secondLocal = SystemManager.getInstance().getSystem("LocalSystem").actorOf
-                (IntActor.props("Second Local"), "secondLocal");
+        ActorRef firstLocal = SystemManager.getInstance().createActor(IntActor.props("First Local"), "firstLocal");
+        ActorRef secondLocal =SystemManager.getInstance().createActor(IntActor.props("Second Local"), "secondLocal");
         firstLocal.tell(new IntMsg(0), secondLocal);
     }
 
     @Test
     public void testSelectionOnLocalSystem() {
         SystemManager.getInstance().createSystem("System", null);
-        SystemManager.getInstance().getSystem("System").actorOf
-                (IntActor.props("First Local"), "firstLocal");
-        SystemManager.getInstance().getSystem("System").actorOf
-                (IntActor.props("Second Local"), "secondLocal");
-        ActorSelection second = SystemManager.getInstance().getSystem("System").actorSelection("akka://System/user/secondLocal");
-        Future<ActorRef> future = SystemManager.getInstance().getSystem("System")
-                .actorSelection("/user/firstLocal").resolveOne(new Timeout(5, TimeUnit.SECONDS).duration());
-        second.tell(new IntMsg(0), future.value().get().get());
+        SystemManager.getInstance().createActor(IntActor.props("First Local"), "firstLocal");
+        SystemManager.getInstance().createActor(IntActor.props("Second Local"), "secondLocal");
+        ActorRef first = SystemManager.getInstance().getLocalActor("firstLocal");
+        ActorRef second = SystemManager.getInstance().getLocalActor("secondLocal");
+        second.tell(new IntMsg(0), first);
     }
 
 
--- core/src/daniele/comunication/SystemManager.java
+++ core/src/daniele/comunication/SystemManager.java
@@ -1,10 +1,19 @@
 package daniele.comunication;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.util.Timeout;
 import com.typesafe.config.Config;
+import javafx.util.Pair;
+import scala.concurrent.Future;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class SystemManager {
     private static SystemManager ourInstance = new SystemManager();
@@ -13,18 +22,34 @@ public class SystemManager {
         return ourInstance;
     }
 
-    private Map<String, ActorSystem> systemMap;
+    private ActorSystem system;
+    private List<Pair<String, ActorRef>> actorList;
 
     private SystemManager() { }
 
     public void createSystem(String systemName, Config config) {
-        if(systemMap == null) {
-            systemMap = new HashMap<>();
-        }
-        this.systemMap.put(systemName, ActorSystem.create(systemName, config));
+        this.system = ActorSystem.create(systemName, config);
     }
 
-    public ActorSystem getSystem(String name) {
-        return this.systemMap.get(name);
+    public ActorRef createActor(Props props, String actorName) {
+        if(this.actorList == null) {
+            this.actorList = new ArrayList<>();
+        }
+        ActorRef ref = this.system.actorOf(props, actorName);
+        this.actorList.add(new Pair<>(actorName, ref));
+        return ref;
+    }
+
+    public ActorRef getLocalActor(String actorName) {
+        for(Pair<String, ActorRef> pair: this.actorList) {
+            if(pair.getKey().equals(actorName)) {
+                return pair.getValue();
+            }
+        }
+        return null;
+    }
+
+    public ActorSelection getRemoteActor(String systemName, String ip, String portNumber, String path) {
+        return this.system.actorSelection("akka.tcp://" + systemName + "@" + ip + ":" + portNumber + path);
     }
 }
