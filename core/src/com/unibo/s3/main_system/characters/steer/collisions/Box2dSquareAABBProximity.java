29b1bce57b9341d23851f086d23abe34ee33b563
mvenditto <venditto.matteo@gmail.com>
2017-08-16 15:22:08 +0200

    Removed unused imports.

--- core/src/com/unibo/s3/main_system/characters/steer/collisions/Box2dSquareAABBProximity.java
+++ core/src/com/unibo/s3/main_system/characters/steer/collisions/Box2dSquareAABBProximity.java
@@ -21,7 +21,6 @@ import com.badlogic.gdx.ai.steer.Steerable;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.QueryCallback;
-import com.badlogic.gdx.ai.steer.Proximity.ProximityCallback;
 import com.badlogic.gdx.physics.box2d.World;
 
 /** A {@code Box2dSquareAABBProximity} is a {@link Proximity} that queries the world for all fixtures that potentially overlap the
--- core/src/com/unibo/s3/main_system/characters/steer/collisions/DetectorsFactory.java
+++ core/src/com/unibo/s3/main_system/characters/steer/collisions/DetectorsFactory.java
@@ -3,7 +3,6 @@ package com.unibo.s3.main_system.characters.steer.collisions;
 import com.badlogic.gdx.ai.steer.Proximity;
 import com.badlogic.gdx.ai.utils.RaycastCollisionDetector;
 import com.badlogic.gdx.math.Vector;
-import com.badlogic.gdx.physics.box2d.World;
 
 public interface DetectorsFactory<T extends Vector<T>> {
 
