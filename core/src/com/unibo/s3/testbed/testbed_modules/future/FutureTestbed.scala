52c45090cddd8f5a62c7cb84ea91e3840f66cdcb
mvenditto <venditto.matteo@gmail.com>
2017-08-23 14:24:53 +0200

    Improved testbed + ui fixes and packages refactoring.

--- core/src/com/unibo/s3/testbed/testbed_modules/future/FutureTestbed.scala
+++ core/src/com/unibo/s3/testbed/testbed_modules/future/FutureTestbed.scala
@@ -1,17 +1,24 @@
-package com.unibo.s3.testbed.testbed_modules
+package com.unibo.s3.testbed.testbed_modules.future
 
+import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.scenes.scene2d.ui.Cell
 import com.badlogic.gdx.scenes.scene2d.ui.Tree.Node
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent
 import com.badlogic.gdx.scenes.scene2d.utils.{ChangeListener, ClickListener}
 import com.badlogic.gdx.scenes.scene2d.{Actor, InputEvent, Stage}
+import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.badlogic.gdx.{Gdx, InputMultiplexer}
 import com.kotcrab.vis.ui.VisUI
+import com.kotcrab.vis.ui.util.ToastManager
+import com.kotcrab.vis.ui.widget.toast.Toast
 import com.kotcrab.vis.ui.widget.{VisLabel, VisTree, _}
 import com.unibo.s3.main_system.AbstractMainApplication
 import com.unibo.s3.testbed.Testbed
+import com.unibo.s3.testbed.testbed_modules.future.ui.KeyHelpTable
 
 trait Sample {
 
@@ -45,7 +52,6 @@ abstract class SampleWithGui extends BaseSample {
   override def init(owner: Testbed): Unit = {
     super.init(owner)
     gui = new Stage(new ScreenViewport())
-    initGui()
   }
 
   override def update(dt: Float): Unit = {
@@ -72,7 +78,7 @@ abstract class SampleWithGui extends BaseSample {
 
   def renderGui(): Unit = if (guiEnabled) gui.draw()
 
-  def initGui(): Unit
+  def initGui(menuTable: VisWindow): Unit
 
 }
 
@@ -134,11 +140,17 @@ case class ScalaTestbed() extends AbstractMainApplication with Testbed {
   private[this] var inputMultiplexer: InputMultiplexer = _
   private[this] var gui: Stage = _
   private[this] var defaultPaneSize = 200f
+  private[this] var currentSampleMenuWidth = 250f
   private[this] var viewport: Cell[_ <: Actor] = _
   private[this] var loadingBar: VisProgressBar = _
+  private[this] var samplePane: VisWindow = _
+  private[this] var lastTransitionOut = false
+  private[this] var toastManager: ToastManager = _
+  private[this] var currSampleName: String = _
+  private[this] var menuBar: MenuBar = _
 
   def resizeGui(): Unit = {
-    viewport.width(Gdx.graphics.getWidth - (2 * defaultPaneSize))
+    viewport.width(Gdx.graphics.getWidth - (currentSampleMenuWidth + defaultPaneSize))
   }
 
   private def addSample(node: Node, sampleName: String): Unit = {
@@ -146,17 +158,53 @@ case class ScalaTestbed() extends AbstractMainApplication with Testbed {
     lab2.addListener(new ClickListener(){
       override def clicked(event: InputEvent, x: Float, y: Float): Unit = {
         super.clicked(event, x, y)
-        nextSample = matchSample(lab2.getText.toString)
+        currSampleName = lab2.getText.toString
+        nextSample = matchSample(currSampleName)
       }
     })
     node.add(new Node(lab2))
   }
 
+  private def createMenu() = {
+    val fileMenu = new Menu("File")
+    val helpMenu = new Menu("Help")
+
+    val keysHelp = new MenuItem("Shortcuts", new ChangeListener {
+      override def changed(event: ChangeEvent, actor: Actor): Unit = {
+        val keys = new KeyHelpTable(true)
+        keys.addKeyBinding("p", "pause.")
+        keys.addKeyBinding("h", "hide left pane.")
+        keys.addKeyBinding("q", "zoom in")
+        keys.addKeyBinding("a", "zoom out")
+        keys.addKeyBinding("arrow-left", "move camera left")
+        keys.addKeyBinding("arrow-right", "move camera right")
+        keys.addKeyBinding("arrow-up", "move camera up")
+        keys.addKeyBinding("arrow-down", "move camera down")
+        keys.addKeyBinding(List("ctrl", "a"), "test combination")
+        val t = new Toast("dark", keys)
+        toastManager.show(t)
+        t.getMainTable.setY(
+          gui.getHeight - t.getMainTable.getPrefHeight - toastManager.getScreenPadding)
+        t.getMainTable.setX((gui.getWidth / 2) - t.getMainTable.getPrefWidth / 2 )
+      }
+    })
+
+    helpMenu.addItem(keysHelp)
+    menuBar.addMenu(fileMenu)
+    menuBar.addMenu(helpMenu)
+
+  }
+
   private def initGui() = {
     VisUI.load()
+    toastManager = new ToastManager(gui)
+    toastManager.setAlignment(Align.bottomRight)
 
     val eastPane = new VisWindow("")
     val westPane = new VisWindow("")
+
+    eastPane.getTitleLabel.setText("Testbed menu")
+    eastPane.getTitleLabel.setColor(new Color(2/255f, 179/255f, 255/255f, 1f))
 
     val tree = new VisTree
     val core = new Node(new VisLabel("Core"))
@@ -166,6 +214,7 @@ case class ScalaTestbed() extends AbstractMainApplication with Testbed {
     addSample(dummy, "Dummy Circle")
     addSample(dummy, "Dummy Circle 2")
     addSample(core, "Entities Playground")
+    addSample(core, "Box2d World")
 
     tree.add(core)
     tree.add(dummy)
@@ -194,18 +243,27 @@ case class ScalaTestbed() extends AbstractMainApplication with Testbed {
     loadingBar.setAnimateDuration(2)
     eastPane.add(loadingBar).fillX().expandX()
 
+    samplePane = new VisWindow("")
+    samplePane.setWidth(defaultPaneSize)
+
+    menuBar = new MenuBar()
+    createMenu()
+
     val root = new VisTable()
     root.setFillParent(true)
-    root.add(westPane).width(defaultPaneSize).fillY().expandY()
+    root.add(menuBar.getTable).fillX().expandX().row()
+    root.add(samplePane).width(currentSampleMenuWidth).fillY().expandY()
     viewport = root.add()
     resizeGui()
-    root.add(eastPane).width(defaultPaneSize).fillY().expandY().row()
+    root.add(eastPane).width(defaultPaneSize).fillY().expandY()
     gui.addActor(root)
   }
 
   private def matchSample(sampleName: String): Option[Sample] = sampleName match {
     case "Dummy Circle" => Option(DummyCircleSample())
     case "Dummy Circle 2" => Option(DummyCircleSample2())
+    case "Entities Playground" => Option(new ScalaEntitySystemModule())
+    case "Box2d World" => Option(new ScalaBox2dModule())
     case _ => None
   }
 
@@ -215,6 +273,14 @@ case class ScalaTestbed() extends AbstractMainApplication with Testbed {
     loadingBar.setAnimateDuration(0.2f)
 
     sample.init(this)
+    samplePane.clear()
+    samplePane.setKeepWithinParent(false)
+    samplePane.setKeepWithinStage(false)
+    sample match {
+      case s: SampleWithGui => s.initGui(samplePane)
+      case _ => ()
+    }
+
     loadingBar.setValue(25)
 
     inputMultiplexer.clear()
@@ -228,6 +294,16 @@ case class ScalaTestbed() extends AbstractMainApplication with Testbed {
     currentSample = Some(sample)
     nextSample = None
     loadingBar.setValue(100)
+
+    val toast = new Toast("dark", new VisTable(true))
+    toast.getContentTable.add(new VisLabel("Loaded module: "))
+    val t = new VisLabel(currSampleName+"!")
+    t.setColor(Color.CORAL)
+    toast.getContentTable.add(t)
+    toastManager.show(toast, 5)
+    toast.getMainTable.setY(
+      gui.getHeight - toast.getMainTable.getPrefHeight - toastManager.getScreenPadding)
+    toast.getMainTable.setX((gui.getWidth / 2) - toast.getMainTable.getPrefWidth / 2 )
   }
 
   override def create(): Unit = {
@@ -273,5 +349,16 @@ case class ScalaTestbed() extends AbstractMainApplication with Testbed {
   override def dispose(): Unit = {
     super.dispose()
     VisUI.dispose()
+  }
+
+  override def keyUp(keycode: Int): Boolean = {
+    super.keyUp(keycode)
+    if (keycode == Keys.H) {
+      samplePane.addAction(
+        Actions.moveTo(if (lastTransitionOut) -samplePane.getWidth else 0,
+          0, 0.30f))
+      lastTransitionOut = !lastTransitionOut
+    }
+    false
   }
 }
---
+++ core/src/com/unibo/s3/testbed/testbed_modules/future/ScalaBox2dModule.scala
@@ -0,0 +1,193 @@
+package com.unibo.s3.testbed.testbed_modules.future
+
+import akka.actor.{ActorRef, Props}
+import akka.pattern.Patterns
+import akka.util.Timeout
+import com.badlogic.gdx.graphics.g2d.{BitmapFont, SpriteBatch}
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.graphics.{Color, Texture}
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.{Body, PolygonShape, World}
+import com.badlogic.gdx.{Gdx, Input, InputMultiplexer}
+import com.kotcrab.vis.ui.widget.VisWindow
+import com.typesafe.config.ConfigFactory
+import com.unibo.s3.InputProcessorAdapter
+import com.unibo.s3.main_system.communication.SystemManager
+import com.unibo.s3.main_system.rendering.ScaleUtils.{getMetersPerPixel, getPixelsPerMeter, metersToPixels, pixelsToMeters}
+import com.unibo.s3.main_system.world.actors._
+import com.unibo.s3.testbed.Testbed
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class ScalaBox2dModule extends SampleWithGui with InputProcessorAdapter {
+
+  private var world: World = _
+  private var worldActor: ActorRef = _
+
+  private var bodyEditorEnabled = false
+  private var topLeft: Option[Vector2] = None
+  private var delta: Option[Vector2] = None
+
+  private var mouseDragged = false
+
+  private var textBatch: SpriteBatch = _
+  private var font: BitmapFont = _
+
+  override def update(dt: Float): Unit = {
+    super.update(dt)
+    worldActor ! Act(dt)
+  }
+
+  override def init(owner: Testbed): Unit = {
+    super.init(owner)
+    textBatch = new SpriteBatch()
+    world = new World(new Vector2(0, 0), true)
+    font = new BitmapFont
+    font.getRegion.getTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
+    font.getData.setScale(1.5f)
+
+    val conf = "{\"akka\":{\"actor\":{\"provider\":\"akka.remote.RemoteActorRefProvider\"}," + "\"loglevel\":\"INFO\",\"remote\":{\"enabled-transports\":[\"akka.remote.netty.tcp\"]" + ",\"log-received-messages\":\"on\",\"log-sent-messages\":\"on\"" + ",\"netty\":{\"tcp\":{\"hostname\":\"" + "127.0.0.1" + "\",\"port\":5050}}}}}"
+    val customConfig = ConfigFactory.parseString(conf)
+    SystemManager.getInstance.createSystem("b2d", customConfig)
+    SystemManager.getInstance.createActor(Props.create(classOf[WorldActor], "world", world), "world")
+    worldActor = SystemManager.getInstance.getLocalActor("world")
+  }
+
+  override def cleanup(): Unit = {
+    super.cleanup()
+    world.dispose()
+    SystemManager.getInstance().shutdownSystem()
+  }
+
+  override def attachInputProcessors(inputMultiplexer: InputMultiplexer): Unit = {
+    super.attachInputProcessors(inputMultiplexer)
+    inputMultiplexer.addProcessor(this)
+  }
+
+  override def initGui(menuTable: VisWindow): Unit = {}
+
+  override def description: String = "Box2d world module."
+
+  private def resetWorld() = worldActor ! ResetWorld
+
+  override def keyUp(keycode: Int): Boolean = {
+
+    if (keycode == Input.Keys.G) {
+      bodyEditorEnabled = !bodyEditorEnabled
+    }
+    if (keycode == Input.Keys.U) {
+      enableGui(!guiEnabled)
+    }
+    false
+  }
+
+  override def touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = {
+    var click = new Vector2(screenX, screenY)
+    click = owner.screenToWorld(click).scl(getMetersPerPixel)
+    topLeft = Option(click)
+    false
+  }
+
+  override def touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = {
+  if (bodyEditorEnabled) {
+    if (mouseDragged) {
+      topLeft.foreach(tl => {
+        delta.foreach(d => {
+          val center = tl.cpy.add(d).scl(0.5f)
+          worldActor.tell(CreateBox(center, new Vector2(Math.abs(tl.x - d.x),
+            Math.abs(tl.y - d.y))), null)
+        })
+      })
+    }
+    if (button == 1) {
+      var click = new Vector2(Gdx.input.getX, Gdx.input.getY)
+      click = owner.screenToWorld(click).scl(getMetersPerPixel)
+      worldActor.tell(DeleteBodyAt(click.x, click.y), null)
+    }
+  }
+  topLeft = None
+  delta = None
+  mouseDragged = false
+  false
+}
+
+  override def touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean = {
+    var click  = new Vector2(screenX, screenY)
+    click = owner.screenToWorld(click)
+    topLeft.foreach(_ => delta = Option(click.cpy.scl(getMetersPerPixel)))
+    mouseDragged = true
+    false
+  }
+
+  override def render(shapeRenderer: ShapeRenderer): Unit = {
+    super.render(shapeRenderer)
+
+    val timeout = new Timeout(Duration.create(5, "seconds"))
+    val future = Patterns.ask(worldActor, new GetAllBodies, timeout)
+    try {
+      val bodies = Await.result(future, timeout.duration).asInstanceOf[com.badlogic.gdx.utils.Array[Body]]
+      for (i <- 0 until bodies.size) {
+        renderBox(shapeRenderer, bodies.get(i), false)
+      }
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+
+    if (bodyEditorEnabled && topLeft.isDefined && delta.isDefined) {
+      shapeRenderer.setColor(Color.GREEN)
+      //shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+      val tmp = topLeft.get.cpy().scl(getPixelsPerMeter)
+      val tmp2 = delta.get.cpy().scl(getPixelsPerMeter)
+      shapeRenderer.rect(tmp.x, tmp.y, tmp2.x - tmp.x, tmp2.y - tmp.y)
+
+      val cx = tmp.x + ((tmp2.x - tmp.x) / 2)
+      val cy = tmp.y + ((tmp2.y - tmp.y) / 2)
+      shapeRenderer.circle(cx, cy, 10)
+
+      val wTextPos = owner.worldToScreen(new Vector2(cx, (cy + (tmp2.y - tmp.y) / 2) - 30))
+      val hTextPos = owner.worldToScreen(new Vector2((cx + (tmp2.x - tmp.x) / 2) + 30, cy))
+
+      if (textBatch != null) {
+        textBatch.begin()
+        font.draw(textBatch, "" + pixelsToMeters((tmp2.x - tmp.x).toInt).round, wTextPos.x, wTextPos.y)
+        font.draw(textBatch, "" + Math.abs(pixelsToMeters((tmp2.y - tmp.y).toInt).round), hTextPos.x, hTextPos.y)
+        textBatch.end()
+      }
+    }
+  }
+
+  private def renderBox (renderer: ShapeRenderer, b: Body, underMouse: Boolean): Unit = {
+    val poly = b.getFixtureList.get(0).getShape.asInstanceOf[PolygonShape]
+    val vertices = new Array[Float](poly.getVertexCount * 2)
+    var j = 0
+
+    for(i <- 0 until poly.getVertexCount) {
+      val v = new Vector2
+      poly.getVertex(i, v)
+      val worldVertex = b.getWorldPoint(v)
+      vertices(j) = metersToPixels(worldVertex.x)
+      vertices(j + 1) = metersToPixels(worldVertex.y)
+      j += 2
+    }
+
+    val c = renderer.getColor
+    if (underMouse) renderer.setColor(Color.CYAN)
+    else renderer.setColor(Color.GRAY)
+
+    val v0 = new Vector2(vertices(0), vertices(1))
+    val t = b.getUserData.asInstanceOf[String].split(":")
+    val s = getPixelsPerMeter
+
+    renderer.setAutoShapeType(true)
+    renderer.set(ShapeRenderer.ShapeType.Filled)
+    renderer.rect(v0.x, v0.y, t(0).toFloat * s, t(1).toFloat * s)
+    renderer.set(ShapeRenderer.ShapeType.Line)
+    renderer.setColor(Color.BLACK)
+    renderer.rect(v0.x, v0.y, t(0).toFloat * s, t(1).toFloat * s)
+    renderer.setAutoShapeType(false)
+    renderer.setColor(c)
+  }
+
+}
