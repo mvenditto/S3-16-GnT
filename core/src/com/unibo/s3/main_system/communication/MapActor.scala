a6e14ff9cf1f44b668aef59c8ff9686c5ea96746
Nicola <nicola.santolini@studio.unibo.it>
2017-08-25 09:49:07 +0200

    Sending map to WorldActor.

--- core/src/com/unibo/s3/main_system/communication/MapActor.scala
+++ core/src/com/unibo/s3/main_system/communication/MapActor.scala
@@ -16,6 +16,7 @@ class MapActor extends UntypedAbstractActor {
     case _: StartMsg =>
       mapGenerator.generate(8, 20, 20, 0, 0) //valori da decidere una volta decise le dimensioni possibili per la mappa
       mapGenerator.getMap.foreach(line => SystemManager.getInstance().getLocalActor("graphActor").tell(MapMsg(line), getSelf()))
+      mapGenerator.getMap.foreach(line => SystemManager.getInstance().getLocalActor("worldActor").tell(MapMsg(line), getSelf()))
     // val file = Gdx.files.local(FILEPATH)
      // file.readString().split("\\n").foreach(line => SystemManager.getInstance().getLocalActor("graphActor").tell(MapMsg(line), getSelf()))
     case _ => println("(mapActor) message unknown:" + message)
