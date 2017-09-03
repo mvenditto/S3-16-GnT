9209c3b7f3e957c4eb702bf0a7e658930b056e87
mvenditto <venditto.matteo@gmail.com>
2017-09-03 15:25:01 +0200

    Changed zoom shortcuts in AbstractMainApplication.

--- core/src/com/unibo/s3/main_system/AbstractMainApplication.java
+++ core/src/com/unibo/s3/main_system/AbstractMainApplication.java
@@ -106,7 +106,7 @@ public abstract class AbstractMainApplication extends ApplicationAdapter impleme
     }
 
     private void handleCameraInput() {
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.R)) {
             cam.zoom += cameraZoomSpeed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
