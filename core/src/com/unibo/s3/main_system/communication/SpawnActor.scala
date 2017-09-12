44a27605a5f3eec9098e68b21b92347ce8212b02
UTENTE <daniele.rosetti@studio.unibo.it>
2017-09-12 13:16:47 +0200

    improved SpawnActor

--- core/src/com/unibo/s3/main_system/communication/SpawnActor.scala
+++ core/src/com/unibo/s3/main_system/communication/SpawnActor.scala
@@ -6,7 +6,7 @@ import com.unibo.s3.main_system.map.{RandomSpawnPointGenerator, SpawnPointGenera
 
 class SpawnActor extends UntypedAbstractActor {
 
-  private[this] val WALL_THICKNESS = 3
+  private[this] var wall_thickness: Int = _
   private[this] val spawnGenerator = new SpawnPointGenerator
   private[this] var map: Array[Array[Int]] = _
 
@@ -15,35 +15,25 @@ class SpawnActor extends UntypedAbstractActor {
   override def onReceive(message: Any): Unit = message match {
     case msg: MapSettingsMsg =>
       this.map = Array.ofDim[Int](msg.width, msg.height)
+      this.wall_thickness = 3
+
     case msg: MapElementMsg =>
       val lineElements = msg.line.split(":").map(_.toFloat)
-      def addWall(lineElements: Array[Float]): Unit = lineElements match {
-        case _ =>
-          if (lineElements.forall(value => value != 0.0 && value != this.map.length+WALL_THICKNESS)) {
-            val x = lineElements(0).toInt-WALL_THICKNESS
-            val y = lineElements(1).toInt-WALL_THICKNESS
-            val width = lineElements(2).toInt
-            val height = lineElements(3).toInt
-            def bottomBorder(start: Int, space: Int): Int = {
-              start-(space-1)/2
-            }
-            def topBorder(start: Int, space: Int): Int = {
-              start+(space-1)/2
-            }
-            for(i <- bottomBorder(x, width) to topBorder(x, width)) {
-              for(j <- this.map.length-topBorder(y, height)-1 until this.map.length-bottomBorder(y, height)-1) {
-                this.map(j)(i) = 1
-              }
-            }
-          }
-          else {
-            this.map.foreach(line => {
-              line.foreach(elem => print(elem))
-              println()
-            })
-          }
+      if (lineElements.forall(value => value != 0.0 && value != this.map.length*this.wall_thickness+this.wall_thickness)) {
+        val x = lineElements(0).toInt
+        val y = lineElements(1).toInt
+        def translation(start: Int): Int = {
+          (start - (this.wall_thickness / 2) - this.wall_thickness) / this.wall_thickness
+        }
+        this.map(translation(x))(translation(y)) = 1
       }
-      addWall(lineElements)
+      else {
+        println()
+        this.map.foreach(line => {
+          line.foreach(elem => print(elem))
+          println()
+        })
+      }
 
     case _: GenerateNewCharacterPositionMsg =>
       //generare la posizione del nuovo agente
