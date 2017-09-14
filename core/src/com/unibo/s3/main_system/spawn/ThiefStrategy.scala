35e0302d7dd0cc3f355346ea684eca753608d10e
UTENTE <daniele.rosetti@studio.unibo.it>
2017-09-14 13:56:17 +0200

    fixed error in ThiefStrategy

--- core/src/com/unibo/s3/main_system/spawn/ThiefStrategy.scala
+++ core/src/com/unibo/s3/main_system/spawn/ThiefStrategy.scala
@@ -1,11 +1,13 @@
 package com.unibo.s3.main_system.spawn
 
 import com.badlogic.gdx.math.Vector2
-import com.unibo.s3.main_system.map.AbstractMapGenerator
+import com.unibo.s3.main_system.game.GameSettings
 
 import scala.util.Random
 
 class ThiefStrategy extends SpawnStrategy {
+
+  private val WALL_THICKNESS = GameSettings.apply().wall_thickness
 
   private[this] var width_shift: Int = _
   private[this] var height_shift: Int = _
@@ -13,9 +15,9 @@ class ThiefStrategy extends SpawnStrategy {
   override def generateSpawnQuadrant(map: Array[Array[Int]]): Vector2 = {
     this.width_shift = map.length / 6
     this.height_shift = map(0).length / 6
-    var x = (map.length / 2) + AbstractMapGenerator.BASE_UNIT
+    var x = (map.length / 2) + WALL_THICKNESS
     if(Random.nextBoolean()) x = x + Random.nextInt(width_shift) else x = x - Random.nextInt(width_shift)
-    var y = (map(0).length / 2) + AbstractMapGenerator.BASE_UNIT
+    var y = (map(0).length / 2) + WALL_THICKNESS
     if(Random.nextBoolean()) y = y + Random.nextInt(height_shift) else y = y - Random.nextInt(height_shift)
 
     new Vector2(x, y)
