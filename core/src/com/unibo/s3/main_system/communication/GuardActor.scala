df68130a04d7c8192588e639212883c02f07d47e
Nicola <nicola.santolini@studio.unibo.it>
2017-09-15 14:02:59 +0200

    Guard and thief develop.

--- core/src/com/unibo/s3/main_system/communication/GuardActor.scala
+++ core/src/com/unibo/s3/main_system/communication/GuardActor.scala
@@ -18,30 +18,36 @@ class GuardActor(private[this] val guard: Guard) extends UntypedAbstractActor {
   override def onReceive(message: Any): Unit = message match {
     case ActMsg(dt) =>
       guard.act(dt)
-      SystemManager.getLocalActor("quadTreeActor").tell(AskNeighboursMsg(this.guard), getSelf())
-      println(log() + "Act received")
+      SystemManager.getLocalActor("quadTreeActor").tell(AskNeighboursWithFovMsg(this.guard), getSelf())
+      //println(log() + "Act received")
       //println(log() + "Current node/destination: " + character.getCurrentNode.getOrElse("Not definied") + "," + character.getCurrentDestination)
-      guard.chooseBehaviour()
-   /* case msg: SendNeighboursMsg =>
-      //refresha vicini
-      character.refreshNeighbours()
-      println("My neighbours are: " + msg.neighbours.filter(neighbour => !neighbour.equals(getSelf())))
-      msg.neighbours.filter(neighbour => !neighbour.equals(getSelf())).foreach(neighbour => neighbour.tell(SendCopInfoMsg(character.getInformation), getSelf()))
-      //msg.neighbours.filter(neighbour => !character.getNeighbours.contains(neighbour)).foreach(neighbour => character.addNeighbour(neighbour))
-      msg.neighbours.filter(neighbour => !neighbour.equals(getSelf())).foreach(neighbour => character.addNeighbour(neighbour))
-      //verifica funzionamento
-      println(log() + "Neibours in list " + character.getNeighbours)
-      println(log() + "Current node/destination " + character.getCurrentNode + "/" + character.getCurrentDestination)
 
+      if(!guard.hasTarget) guard.chooseBehaviour()
+
+    case msg: SendNeighboursMsg =>
+      //refresha vicini
+      //character.refreshNeighbours()
+
+      println(log + "My neighbours are: " + msg.neighbours.filter(neighbour => !neighbour.equals(getSelf())))
+
+      msg.neighbours.filter(neighbour => !neighbour.equals(getSelf())).foreach(neighbour => guard.addNeighbour(neighbour))
+      ///msg.neighbours.filter(neighbour => !neighbour.equals(getSelf())).foreach(neighbour => neighbour.tell(SendCopInfoMsg(character.getInformation), getSelf()))
+      //msg.neighbours.filter(neighbour => !character.getNeighbours.contains(neighbour)).foreach(neighbour => character.addNeighbour(neighbour))
+      //msg.neighbours.filter(neighbour => !neighbour.equals(getSelf())).foreach(neighbour => character.addNeighbour(neighbour))
+      //verifica funzionamento
+      //println(log() + "Neibours in list " + character.getNeighbours)
+      //println(log() + "Current node/destination " + character.getCurrentNode + "/" + character.getCurrentDestination)
+/*
     case msg: SendCopInfoMsg =>
-      println(log() + "Infos received")
-      println("cop: " + getSelf() + "| info from: " + getSender() + ", visited vertices: " + msg.visitedVertices)
       character.updateGraph(msg.visitedVertices)
-      println("cop: " + getSelf() + " known vertices: " + character.getInformation)
-      //qui ho le info dell'altro poliziotto quindi poi posso fare quello che devo
 */
-    case msg: SendGraphMsg=>
-      System.out.println(log() + "Initial graph received")
+    case SendThievesInProximityMsg(thieves) =>
+      thieves.foreach(t => println(log() + "Ladro! " + t))
+      guard.chooseTarget(thieves)
+
+
+    case msg: SendGraphMsg =>
+      println(log() + "Initial graph received")
       println("cop: " + getSelf() + "| received graph")
       guard.setGraph(msg.graph)
 
