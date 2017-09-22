2f70a4e8619b07b850e9fe866ff04727f403663c
Sara <sara.sintoni4@studio.unibo.it>
2017-09-22 12:42:11 +0200

    incremented timeout

--- core/src/com/unibo/s3/main_system/communication/CommunicatorActor.scala
+++ core/src/com/unibo/s3/main_system/communication/CommunicatorActor.scala
@@ -1,6 +1,5 @@
 package com.unibo.s3.main_system.communication
 
-import java.beans.{PropertyChangeEvent, PropertyChangeListener, VetoableChangeListener}
 import java.beans.{PropertyChangeEvent, PropertyChangeListener}
 import java.net.InetAddress
 
@@ -17,7 +16,7 @@ import scala.concurrent.{Await, TimeoutException}
 
 class CommunicatorActor extends UntypedAbstractActor{
 
-  implicit val timeout: Timeout = Timeout(1 seconds)
+  implicit val timeout: Timeout = Timeout(1.5 seconds)
 
   def showDialog(): Unit = {
     import javax.swing.JOptionPane
@@ -34,7 +33,6 @@ class CommunicatorActor extends UntypedAbstractActor{
   override def onReceive(message: Any): Unit = message match {
     case _:AskIPMsg => val ref = SystemManager.getRemoteActor(AkkaSystemNames.ComputeSystem, "/user/",
       GeneralActors.COMMUNICATOR_ACTOR.name)
-      println("ref = " + ref.toString())
       val future = ref ? SendIPMsg(InetAddress.getLocalHost.getHostAddress)
       try {
         Await.result(future, timeout.duration).asInstanceOf[ACKComputationNode]
@@ -42,7 +40,7 @@ class CommunicatorActor extends UntypedAbstractActor{
         case _: TimeoutException => showDialog()
       }
 
-    case msg:SendIPMsg => println("IP ricevuto")
+    case msg:SendIPMsg =>
       SystemManager.setIPForRemoting(msg.IP, GUISystemPort)
       sender() ! ACKComputationNode()
 
